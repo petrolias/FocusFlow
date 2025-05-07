1afdbcb5bc7cbd66d0e9871cb32a33c06f8136ae
Petrolias Christoforos <petroliaschristopher@gmail.com>
2025-05-07 22:24:24 +0300

    more updates

--- FocusFlow.Abstractions/Api/Shared/ProjectsDtos.cs
+++ FocusFlow.Abstractions/Api/Shared/ProjectsDtos.cs
@@ -1,4 +1,5 @@
-﻿using FocusFlow.Abstractions.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using FocusFlow.Abstractions.Models;
 
 namespace FocusFlow.Abstractions.Api.Shared
 {
@@ -10,7 +11,11 @@ namespace FocusFlow.Abstractions.Api.Shared
 
     public record ProjectDto : EntryRecordBase
     {
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(100, ErrorMessage = "Title can't exceed 100 characters")]
         public string Name { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description can be at most 500 characters")]
         public string? Description { get; set; }
         public ICollection<TaskItemDto>? Tasks { get; set; } = [];        
     }
