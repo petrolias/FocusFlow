aa869dcbefc8d64046534e9131d07ecb7da2b537
Petrolias Christoforos <petroliaschristopher@gmail.com>
2025-05-07 23:18:28 +0300

    tasks ui updates

--- FocusFlow.Abstractions/Api/Shared/ProjectsDtos.cs
+++ FocusFlow.Abstractions/Api/Shared/ProjectsDtos.cs
@@ -11,8 +11,8 @@ namespace FocusFlow.Abstractions.Api.Shared
 
     public record ProjectDto : EntryRecordBase
     {
-        [Required(ErrorMessage = "Title is required")]
-        [MaxLength(100, ErrorMessage = "Title can't exceed 100 characters")]
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100, ErrorMessage = "Name can't exceed 100 characters")]
         public string Name { get; set; }
 
         [StringLength(500, ErrorMessage = "Description can be at most 500 characters")]
--- FocusFlow.Abstractions/Api/Shared/TaskItemDtos.cs
+++ FocusFlow.Abstractions/Api/Shared/TaskItemDtos.cs
@@ -1,4 +1,5 @@
-﻿using FocusFlow.Abstractions.Constants;
+﻿using System.ComponentModel.DataAnnotations;
+using FocusFlow.Abstractions.Constants;
 using FocusFlow.Abstractions.Models;
 
 namespace FocusFlow.Abstractions.Api.Shared
@@ -16,15 +17,21 @@ namespace FocusFlow.Abstractions.Api.Shared
 
     public record TaskItemDto : EntryRecordBase
     {
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(100, ErrorMessage = "Title can't exceed 100 characters")]
         public string Title { get; set; }
+        [StringLength(500, ErrorMessage = "Description can be at most 500 characters")]
         public string? Description { get; set; }
         public DateTimeOffset? DueDate { get; set; }
+        [Required(ErrorMessage = "Status is required")]
         public TaskItemStatusEnum? Status { get; set; }
+        [Required(ErrorMessage = "Priority is required")]
         public TaskItemPriorityEnum? Priority { get; set; }
         public string? AssignedUserId { get; set; }
         public string? AssignedUserName { get; set; }
         public Guid? ProjectId { get; set; }
         public string ProjectInfo() => string.IsNullOrEmpty(Project?.Name) ? "Not available" : Project.Name;
+        public string DueDateFormatted() => DueDate == null || DueDate == DateTimeOffset.MinValue ? "Not available" : DueDate.Value.ToString("yyyy-MM-dd");
 
         public ProjectDtoBase? Project { get; set; }
     }
